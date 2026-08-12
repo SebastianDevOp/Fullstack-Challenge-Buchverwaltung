aeafe1d5bbf7e2806456d84d223d01327faa70f8
Sebastian Kramer Garfias <DevOpSeba@proton.me>
2026-08-12 09:27:25 +0200

    Add endpoint to Kotlin Service

---
+++ backend/book-api/src/main/kotlin/de/bookmanager/book_api/HealthController.kt
@@ -0,0 +1,13 @@
+package de.bookmanager.book_api
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class HealthController {
+
+    @GetMapping("/api/health")
+    fun health(): Map<String, String> = mapOf("status" to "ok")
+    
+}
