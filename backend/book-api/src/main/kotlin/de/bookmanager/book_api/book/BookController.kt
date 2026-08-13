c956b9991573c337271efc92e875dcfe0a80b82d
Sebastian Kramer Garfias <DevOpSeba@proton.me>
2026-08-13 15:35:45 +0200

    Add DELETE /api/books/{id}

--- backend/book-api/src/main/kotlin/de/bookmanager/book_api/book/BookController.kt
+++ backend/book-api/src/main/kotlin/de/bookmanager/book_api/book/BookController.kt
@@ -6,6 +6,7 @@ import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -104,5 +105,18 @@ class BookController(val bookRepository: BookRepository, val authorRepository: A
                 bookRepository.save(bookToUpdate)
 
                 return bookToUpdate.toDto()
+        }
+
+        @DeleteMapping("/{id}")
+        @ResponseStatus(HttpStatus.NO_CONTENT)
+        fun deleteBook(@PathVariable("id") id: Int) {
+
+                val bookToDelete: Book? = bookRepository.findBookById(id)
+
+                if (bookToDelete == null) {
+                        throw ResponseStatusException(HttpStatus.NOT_FOUND)
+                }
+
+                bookRepository.delete(bookToDelete)
         }
 }
